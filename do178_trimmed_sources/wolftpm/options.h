202c31e1f2645ba68daac2eb561637bcfae30968
Tesfa Mael <tesfa@wolfssl.com>
2025-04-03 10:22:14 -0700

    Add an empty options.h

---
+++ do178_trimmed_sources/wolftpm/options.h
@@ -0,0 +1,18 @@
+
+#ifndef WOLFMTPM_OPTIONS_H
+#define WOLFMTPM_OPTIONS_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* empty */
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* WOLFMTPM_OPTIONS_H */
+
